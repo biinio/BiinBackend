35cc177c779eec9fb7067de55d9bd31fb12edf14
Luis Bonilla Hernandez <luisbonillah@gmail.com>
2014-05-02 21:24:21 -0600

    add partial jade index

--- app.js
+++ app.js
@@ -15,13 +15,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(express.static(__dirname + '/public'));
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+console.log(process.cwd());
+
+app.use(express.static(process.cwd() + '/public'));
 app.use(app.router);
 
 app.get('/', routes.index);
