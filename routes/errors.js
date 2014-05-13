dbf1cd9eae8733d3a0883c478122d8437c9e9eff
Luis Bonilla Hernandez <luisbonillah@gmail.com>
2014-05-13 16:00:41 -0600

    view changes

--- routes/errors.js
+++ routes/errors.js
@@ -5,9 +5,7 @@ module.exports = function(db){
 	//Get Erros List
 	functions.index = function(req,res){
 		error.find({},'',function(err,data){
-			console.log("Error data:"+data);
-			res.json(data);
-			//res.render('error/index', { title: 'Organizations list' ,user:req.user,errors:data});		
+			res.render('error/index', { title: 'Organizations list' ,user:req.user,errors:data});		
 
 		})		
 	}
