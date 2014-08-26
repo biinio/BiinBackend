568515902ecb9fafe34e8d9edd186198b6b5be34
Luis Bonilla Hernandez <luisbonillah@gmail.com>
2014-08-26 14:49:49 -0600

    Changed mongoose driver

--- biin_modules/db.js
+++ biin_modules/db.js
@@ -1,4 +1,3 @@
-console.log("THE DB CONNECTION IS: "+ process.env.DB_CONNECTION);
 var mongoose = require('mongoose');
 mongoose.connect(process.env.DB_CONNECTION);
 module.exports = mongoose.connection;
