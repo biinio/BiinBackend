2c35d92546cd27d024a5553983035ec8fca6401b
Luis Bonilla Hernandez <luisbonillah@gmail.com>
2015-06-15 09:37:02 -0600

    Changes in Search site constraint

--- schemas/searchSiteCategory.js
+++ schemas/searchSiteCategory.js
@@ -19,5 +19,6 @@ var siteCatObj = {
 var siteCatSchema = new Schema(siteCatObj);
 siteCatSchema.index({ min_latitude: 1, min_logitude: 1,max_latitude:1,max_logitude:1,categoryIdentifier:1 }, { unique: true })
 siteCatSchema.index({ 'sites.proximity': 1, identifier: 1})
+siteCatSchema.index({ 'sites.identifier':1}, {unique: true})
 module.exports = mongoose.model('searchSiteCategories', siteCatSchema);
 
