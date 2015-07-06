068b398c1ff81b185a14c80956a918eb1a384065
isbiin <isalazar@biinapp.com>
2015-07-06 08:52:15 -0600

    adding days buttons

--- public/javascripts/angular/dashboardgraphcontroller/VisitsController.js
+++ public/javascripts/angular/dashboardgraphcontroller/VisitsController.js
@@ -23,93 +23,100 @@ biinAppVisitsGraph.controller("biinAppVisitsGraphController", ['$scope', '$http'
             var stringDate = yyyy + '-' + mm + '-' + dd;
             return stringDate;
         }
+        
+        $scope.getChartData = function ( days )
+        {
+            var today = new Date();
+            var previusDate = new Date();
+            previusDate.setTime(today.getTime() - days * 86400000);
 
-        var today = new Date();
-        var monthAgo = new Date();
-        monthAgo.setTime(today.getTime() - 30 * 86400000);
 
-
-        $http.get('/api/dashboard/visits', {
-            headers: {
-                organizationid: $scope.organizationId,
-                endDate: getDateString(today),
-                startDate: getDateString(monthAgo)
-            }
-        }).success(function(data) {
-            var visits = [];
-            var keys = Object.keys(data.data);
-            var maxValue = 1;
-            for (var i = 0; i < keys.length; i++) {
-                var s = new Date(keys[i]); 
-                visits.push({
-                    x: s.getTime(),
-                    y: data.data[keys[i]]
-                });
-                if(data.data[keys[i]] > maxValue )
-                    maxValue = data.data[keys[i]];
-            }
-
-            $scope.data = [{
-                values: visits,
-                key: 'visits',
-                color: '#006699',
-                area: true
-            }];
-
-            $scope.options = {
-                chart: {
-                    type: 'lineChart',
-                    height: 450,
-                    margin: {
-                        top: 20,
-                        right: 20,
-                        bottom: 40,
-                        left: 55
-                    },
-                    x: function(d) {
-                        return d.x;
-                    },
-                    y: function(d) {
-                        return d.y;
-                    },
-                    useInteractiveGuideline: true,
-                    dispatch: {
-                        stateChange: function(e) {
-                            console.log("stateChange");
-                        },
-                        changeState: function(e) {
-                            console.log("changeState");
-                        },
-                        tooltipShow: function(e) {
-                            console.log("tooltipShow");
-                        },
-                        tooltipHide: function(e) {
-                            console.log("tooltipHide");
-                        }
-                    },
-                    xAxis: {
-                        axisLabel: 'Date',
-                        tickFormat: function(d) {
-                            return d3.time.format('%d-%m-%y')(new Date(d));
-                        },
-                        showMaxMin:false,
-                        axisLabelDistance: 30
-                    },
-                    yAxis: {
-                        axisLabel: 'Visits',
-                    },
-                    callback: function(chart) {
-                        console.log("!!! lineChart callback !!!");
-                    },
-                    forceY:[0,maxValue]
-                },
-                title: {
-                    enable: true,
-                    text: 'Graph 1'
+            $http.get('/api/dashboard/visits', {
+                headers: {
+                    organizationid: $scope.organizationId,
+                    endDate: getDateString(today),
+                    startDate: getDateString(previusDate)
                 }
-            };
-        });
+            }).success(function(data) {
+                var visits = [];
+                var keys = Object.keys(data.data);
+                var maxValue = 1;
+                for (var i = 0; i < keys.length; i++) {
+                    var s = new Date(keys[i]); 
+                    visits.push({
+                        x: s.getTime(),
+                        y: data.data[keys[i]]
+                    });
+                    if(data.data[keys[i]] > maxValue )
+                        maxValue = data.data[keys[i]];
+                }
 
+                $scope.data = [{
+                    values: visits,
+                    key: 'visits',
+                    color: '#006699',
+                    area: true
+                }];
+
+                $scope.options = {
+                    chart: {
+                        type: 'lineChart',
+                        height: 450,
+                        margin: {
+                            top: 20,
+                            right: 20,
+                            bottom: 40,
+                            left: 55
+                        },
+                        x: function(d) {
+                            return d.x;
+                        },
+                        y: function(d) {
+                            return d.y;
+                        },
+                        useInteractiveGuideline: true,
+                        dispatch: {
+                            stateChange: function(e) {
+                                console.log("stateChange");
+                            },
+                            changeState: function(e) {
+                                console.log("changeState");
+                            },
+                            tooltipShow: function(e) {
+                                console.log("tooltipShow");
+                            },
+                            tooltipHide: function(e) {
+                                console.log("tooltipHide");
+                            }
+                        },
+                        xAxis: {
+                            axisLabel: 'Date',
+                            tickFormat: function(d) {
+                                return d3.time.format('%d-%m-%y')(new Date(d));
+                            },
+                            showMaxMin:false,
+                            axisLabelDistance: 30
+                        },
+                        yAxis: {
+                            axisLabel: 'Visits',
+                        },
+                        callback: function(chart) {
+                            console.log("!!! lineChart callback !!!");
+                        },
+                        forceY:[0,maxValue]
+                    },
+                    title: {
+                        enable: true,
+                        text: 'Graph 1'
+                    }
+                };
+            });
+        }
+        
+        $scope.changeChartRange = function( days ){
+            $scope.getChartData(days);
+        }
+        $scope.getChartData(30);
         //Turn off the Loader
         turnLoaderOff();
 
