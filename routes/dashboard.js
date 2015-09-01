b30bfd4041176fc03f1af562c4dcbe8eef0823e6
isbiin <isalazar@biinapp.com>
2015-09-01 14:47:35 -0600

    removing all logic for notification report by now

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -200,19 +200,19 @@ module.exports = function(){
 									visits = visits.concat(compressedVisits[compressedVisitsKeys[i]]);
 								}*/
 								
-								for (i = 0; i < actions.length; i++) 
+								/*for (i = 0; i < actions.length; i++) 
 								{
 									actions[i].at = actions[i].at.indexOf("T") == -1 ?  actions[i].at.split(" ")[0] : actions[i].at.split("T")[0];
-								};
+								};*/
 
 								
 								//TODO: change date schema type from string to longInteger
-								var datesKeys = Object.keys(counterDates);
+								/*var datesKeys = Object.keys(counterDates);
 								for (i = 0; i < actions.length; i++) 
 								{
 									if(datesKeys.indexOf(actions[i].at) > -1)
 										counterDates[actions[i].at] += 1;
-								};
+								};*/
 								res.json({"data":counterDates});
 							}
 						});
