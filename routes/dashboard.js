514bc0ebc7fceef90b9b9b814ba3a346d335606f
isbiin <isalazar@biin.io>
2015-11-03 14:10:55 -0600

    working on new visits win mobile

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -406,7 +406,7 @@ var UNFOLLOW_SITE = "17";
             var idUsersVisits = _.pluck(visitsData, '_id');
             var idUsersOldVisits = _.pluck(oldVisitsData, '_id');
             var newVisits = _.difference(idUsersVisits,idUsersOldVisits);
-            res.json({data:{news:newVisits.length}});
+            res.json({data:newVisits.length});
           });
       });
   }
