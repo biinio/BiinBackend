01aac5bf779324981053289dfb3f3ff9c28cb0dc
isbiin <isalazar@biin.io>
2015-12-02 09:28:02 -0600

    adding to the json the call to action

--- routes/mobileEndPoint.js
+++ routes/mobileEndPoint.js
@@ -157,6 +157,10 @@ module.exports = function(){
     elementValidated.userLiked = element.userLiked? element.userLiked : "0";
     elementValidated.userCollected = element.userCollected? element.userCollected : "0";
     elementValidated.userViewed = element.userViewed? element.userViewed : "0";
+    elementValidated.hasCallToAction = "1";
+    elementValidated.callToActionURL = "http//:www.google.com";
+    elementValidated.callToActionTitle = "GOOGLE";
+    
     return elementValidated;
   }
 
