de16b4385aecf035affb8d0557393386298b6509
isbiin <isalazar@biin.io>
2016-01-20 23:54:14 -0600

    no message

--- server.js
+++ server.js
@@ -11,16 +11,13 @@ var fs= require('fs'),
 
 //Define local vars
 var isDevelopment = process.env.NODE_ENV === 'development';
-var httpPort = process.env.PORT || 3000;
-var httpsPort = 8443;
 
-app.set('port',httpPort);
 
 //Https credentials
 //var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8').toString();;
 //var certificate = fs.readFileSync('sslcert/bundle.crt', 'utf8').toString();;
 // /var credentials = {key: privateKey, cert: certificate};
-
+var httpPort = process.env.PORT || 5000;
 //Http Server instance
 var httpServer = http.createServer(app).listen(httpPort,function(){
 	console.log("Listing server");
