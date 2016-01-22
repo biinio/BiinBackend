d52085592d2fc57ce27633a6d11963f9bfc888b1
isbiin <isalazar@biin.io>
2016-01-22 09:45:08 -0600

    fixing problem with call to action

--- routes/mobileEndPoint.js
+++ routes/mobileEndPoint.js
@@ -209,9 +209,9 @@ module.exports = function () {
         elementValidated.userLiked = element.userLiked ? element.userLiked : "0";
         elementValidated.userCollected = element.userCollected ? element.userCollected : "0";
         elementValidated.userViewed = element.userViewed ? element.userViewed : "0";
-        elementValidated.hasCallToAction = elementValidated.hasCallToAction?elementValidated.hasCallToAction:"0";
-        elementValidated.callToActionURL = elementValidated.callToActionURL?elementValidated.callToActionURL:"";
-        elementValidated.callToActionTitle = elementValidated.callToActionTitle? elementValidated.callToActionTitle : "";
+        elementValidated.hasCallToAction = element.hasCallToAction?element.hasCallToAction:"0";
+        elementValidated.callToActionURL = element.callToActionURL?element.callToActionURL:"";
+        elementValidated.callToActionTitle = element.callToActionTitle? element.callToActionTitle : "";
 
 
         return elementValidated;
