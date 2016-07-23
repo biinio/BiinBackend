e6c2aa26808ae9185d682a7e07b23c30f68980fa
isbiin <isalazar@biin.io>
2016-07-23 09:27:43 -0600

    fixing problems with image upload

--- app/routes/api/organization.api.router.server.js
+++ app/routes/api/organization.api.router.server.js
@@ -7,12 +7,14 @@
 module.exports = function (app) {
     // Organization Routes
     var api = require('../../controllers/api.server.controller');
+    var multipart = require('connect-multiparty');
+    var multipartMiddleware = multipart();
 
     //Organization Routes
     app.route('/api/organizations').get(api.listOrganizations);
     app.route('/api/organizations/:identifier').post(api.setOrganization);
     app.route('/api/organizations/:accountIdentifier').put(api.createOrganization);
-    app.route('/api/organizations/:identifier/image').post(api.uploadImage);
+    app.route('/api/organizations/:identifier/image').post(multipartMiddleware,api.uploadImage);
     app.route('/api/organizations/:identifier').delete(api.markAsDeleted);
     app.route('/api/organizations/:identifier/:siteIdentifier/minor').get(api.getMinor);
     app.route('/api/organizations/:identifier/checkImage/:imageIdentifier').get(api.checkImageUse);
