16769e98728418783ecd5ddf10bda0a9b91812d1
isbiin <isalazar@biin.io>
2016-08-29 14:45:30 -0600

    clean up code

--- app/routes/api/clients.api.router.server.js
+++ app/routes/api/clients.api.router.server.js
@@ -15,21 +15,12 @@ module.exports = function (app) {
 
     app.post('/api/loginCMS', api.loginCMS);
 
-
-
     app.route('/api/account').put(api.updateClient);
     app.route('/api/account').get(api.listClient);
-
-
-    //app.route('/api/account').put(api.set);
-    //app.route('/api/account').get(api.list);
-
-
 
     app.route('/api/clients/invite').post(api.inviteNewClient);
     app.route('/api/clients/organization/:idorganization').get(api.getClientsByOrganization);
     app.route('/api/clients/:identifier/organization/:idorganization').delete(api.removeClientFromOrganization);
-
 
     app.route('/api/clients/upgrade/organization').get(api.upgradeOrganizationManagement);
 
