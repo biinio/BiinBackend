019f3267e7f2c6a6a36cfbac31d982b133c24b15
isbiin <isalazar@biin.io>
2016-09-07 10:23:23 -0600

    Commit new user update and register flow

--- app/controllers/mobile/user.mobile.server.controller.js
+++ app/controllers/mobile/user.mobile.server.controller.js
@@ -1021,7 +1021,6 @@ exports.deleteMobileCollectSiteToCollection = function (req, res) {
     })
 }
 
-
 //Update by mobile Id
 exports.updateMobile = function (req, res) {
 
@@ -1125,83 +1124,29 @@ exports.updateMobile = function (req, res) {
                 delete modelToReturn.facebookId;
                 delete modelToReturn.accountState;
                 res.json({data: modelToReturn, status: "0", result: "1"});
-
-                /*sendVerificationMail(req, model, function () {
-                    var modelToReturn = model;
-                    modelToReturn.birthDate = birthDate.replace("T", " ").replace("Z", "");
-                    modelToReturn.isEmailVerified = modelToReturn.accountState ? "1" : "0";
-                    modelToReturn.facebook_id = facebookId;
-                    delete modelToReturn.facebookId;
-                    delete modelToReturn.accountState;
-                    res.json({data: modelToReturn, status: "0", result: "1"});
-                })*/
             }
         })
     };
 
-
     if (model) {
-        mobileUser.findOne({'biinName': model.email}, function (err, foundEmail) {
-            if (err)
-                res.json({data: {}, status: "5", result: "0"});
-            else if (typeof(foundEmail) === "undefined" || foundEmail === null) {
-                createNewUser(model);
-            } else {
-                if (identifier == "none")
-                    identifier = foundEmail.identifier;
-                updateModel(model);
-            }
-        })
+        if(identifier=="none"){
+            //Crear Usuario
+            mobileUser.findOne({'biinName': model.email}, function (err, foundEmail) {
+                if (err)
+                    res.json({data: {}, status: "5", result: "0"});
+                else if (typeof(foundEmail) === "undefined" || foundEmail === null) {
+                    createNewUser(model);
+                } else {
+                    res.json({data: {}, status: "5", result: "0"});
+                }
+            });
+        } else {
+            updateModel(model);
+        }
     } else {
         res.json({data: {}, status: "5", result: "0"});
     }
 
-
-};
-
-//Get the authentication of the user **To change **Deprecated
-exports.login = function (req, res) {
-    var user = req.params.user;
-    var password = req.params.password;
-
-    mobileUser.findOne({'biinName': user}, function (err, foundBinnie) {
-        if (err)
-            res.json({data: {identifier: ""}, status: "5", result: "0"});
-        else {
-            var result = typeof(foundBinnie) !== 'undefined' && foundBinnie !== null;
-            var identifier = "";
-            if (result) {
-                foundBinnie.comparePassword(password, function (err, isMath) {
-                    identifier = foundBinnie.identifier;
-                    var isMathToString = isMath ? "1" : "0";
-                    var code = isMath ? "0" : "8";
-                    res.json({data: {identifier: identifier}, status: code, result: isMathToString});
-                });
-            } else {
-                res.json({data: {identifier: identifier}, status: "7", result: "0"});
-            }
-        }
-    });
-};
-
-//Get the authentication of the user **To change **Deprecated
-exports.loginFacebook = function (req, res) {
-    var user = req.params.user;
-
-    mobileUser.findOne({'biinName': user}, function (err, foundBinnie) {
-        if (err)
-            res.json({data: {identifier: ""}, status: "5", result: "0"});
-        else {
-            var result = typeof(foundBinnie) !== 'undefined' && foundBinnie !== null;
-            var identifier = "";
-            if (result) {
-                identifier = foundBinnie.identifier;
-                res.json({data: {identifier: identifier}, status: "0", result: "1"});
-            } else {
-                res.json({data: {identifier: identifier}, status: "7", result: "0"});
-            }
-        }
-    });
 };
 
 //GET/POST the activation of the user
